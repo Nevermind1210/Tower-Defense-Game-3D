a4ae4c732dd927a38d60ced55619044f366016de
Xavier Arias <xavier_rate@outlook.com>
2021-06-18 16:27:34 +1000

    Settings screen implemeneted....

--- Assets/Scripts - Xavier Arias/UIElemntsScripts/OptionsMenu.cs
+++ Assets/Scripts - Xavier Arias/UIElemntsScripts/OptionsMenu.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -9,19 +10,17 @@ namespace UIStuff
     public class OptionsMenu : MonoBehaviour
     {
         public AudioMixer audioMixer;
-        public Dropdown resolutionDropdown;
-        public Dropdown qualityDropdown;
-        public Dropdown textureDropdown;
-        public Dropdown aaDropdown;
+        public TMP_Dropdown resolutionDropdown;
+        public TMP_Dropdown qualityDropdown;
         public Slider volumeSlider;
         float currentVolume;
         Resolution[] resolutions;
 
         public void Start()
         {
+            resolutions = Screen.resolutions;
             resolutionDropdown.ClearOptions();
             List<string> options = new List<string>();
-            resolutions = Screen.resolutions;
             int currentResolutionIndex = 0;
             
             for (int i = 0; i < resolutions.Length; i++)
@@ -29,19 +28,16 @@ namespace UIStuff
                 string option = resolutions[i].width + " x " + 
                                 resolutions[i].height;
                 options.Add(option);
-                if (resolutions[i].width == Screen.currentResolution.width 
-                    && resolutions[i].height == Screen.currentResolution.height)
-                    currentResolutionIndex = i;
-            }
-            
-            resolutionDropdown.AddOptions(options);
-            resolutionDropdown.RefreshShownValue();
-            LoadSettings(currentResolutionIndex);
-        }
 
-        public void Update()
-        {
-            
+                if (resolutions[i].width == Screen.currentResolution.width &&
+                    resolutions[i].height == Screen.currentResolution.height)
+                {
+                    currentResolutionIndex = i;
+                }
+            }
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
         }
 
         public void SetVolume(float volume)
@@ -55,58 +51,9 @@ namespace UIStuff
             Screen.fullScreen = isFullscreen;
         }
         
-        public void SetResolution(int resolutionIndex)
-        {
-            Resolution resolution = resolutions[resolutionIndex];
-            Screen.SetResolution(resolution.width, 
-                resolution.height, Screen.fullScreen);
-        }
-        
-        public void SetTextureQuality(int textureIndex)
-        {
-            QualitySettings.masterTextureLimit = textureIndex;
-            qualityDropdown.value = 6;
-        }
-        
-        public void SetAntiAliasing(int aaIndex)
-        {
-            QualitySettings.antiAliasing = aaIndex;
-            qualityDropdown.value = 6;
-        }
-        
         public void SetQuality(int qualityIndex)
         {
-            if (qualityIndex != 6) // if the user is not using 
-                //any of the presets
-                QualitySettings.SetQualityLevel(qualityIndex);
-            switch (qualityIndex)
-            {
-                case 0: // quality level - very low
-                    textureDropdown.value = 3;
-                    aaDropdown.value = 0;
-                    break;
-                case 1: // quality level - low
-                    textureDropdown.value = 2;
-                    aaDropdown.value = 0;
-                    break;
-                case 2: // quality level - medium
-                    textureDropdown.value = 1;
-                    aaDropdown.value = 0;
-                    break;
-                case 3: // quality level - high
-                    textureDropdown.value = 0;
-                    aaDropdown.value = 0;
-                    break;
-                case 4: // quality level - very high
-                    textureDropdown.value = 0;
-                    aaDropdown.value = 1;
-                    break;
-                case 5: // quality level - ultra
-                    textureDropdown.value = 0;
-                    aaDropdown.value = 2;
-                    break;
-            }
-            qualityDropdown.value = qualityIndex;
+            QualitySettings.SetQualityLevel(qualityIndex);
         }
         
         public void SaveSettings()
@@ -115,10 +62,6 @@ namespace UIStuff
                 qualityDropdown.value);
             PlayerPrefs.SetInt("ResolutionPreference", 
                 resolutionDropdown.value);
-            PlayerPrefs.SetInt("TextureQualityPreference", 
-                textureDropdown.value);
-            PlayerPrefs.SetInt("AntiAliasingPreference", 
-                aaDropdown.value);
             PlayerPrefs.SetInt("FullscreenPreference", 
                 Convert.ToInt32(Screen.fullScreen));
             PlayerPrefs.SetFloat("VolumePreference", 
@@ -137,16 +80,6 @@ namespace UIStuff
                     PlayerPrefs.GetInt("ResolutionPreference");
             else
                 resolutionDropdown.value = currentResolutionIndex;
-            if (PlayerPrefs.HasKey("TextureQualityPreference"))
-                textureDropdown.value = 
-                    PlayerPrefs.GetInt("TextureQualityPreference");
-            else
-                textureDropdown.value = 0;
-            if (PlayerPrefs.HasKey("AntiAliasingPreference"))
-                aaDropdown.value = 
-                    PlayerPrefs.GetInt("AntiAliasingPreference");
-            else
-                aaDropdown.value = 1;
             if (PlayerPrefs.HasKey("FullscreenPreference"))
                 Screen.fullScreen = 
                     Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
